33def82a94a3b5fd8c91fde332565fce9edfa785
Aaron <amartinezdasilva@danielcastelao.org>
2017-02-02 13:54:41 +0100

    marca colocada

--- app/src/main/java/com/example/aaron/proyecto2/MapsActivity.java
+++ app/src/main/java/com/example/aaron/proyecto2/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng vigo = new LatLng(42.23776344437179, -8.714438080787659 );
+        mMap.addMarker(new MarkerOptions().position(vigo).title("MARCA"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(vigo));
     }
 }
